5b34d19936cc82364db952dab4a30c003ad0417f
sk <110441744+shunkitano@users.noreply.github.com>
2022-11-01 17:39:34 +0900

    commit1101

--- src/components/SignUpComp.vue
+++ src/components/SignUpComp.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
-    <div class="signup">
-      <button @click="close" class="close">
+    <div class="signup" :class="{isActive:isDrag, isActive2:isClose}">
+      <button @click="close" @mouseenter="dragIn" class="close">
         <svg width="26" height="25" viewBox="0 0 26 25" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" clip-rule="evenodd" d="M23.9598 1.74696C24.5456 2.33274 24.5456 3.28249 23.9598 3.86828L4.86796 22.9602C4.28218 23.5459 3.33243 23.5459 2.74664 22.9602C2.16086 22.3744 2.16086 21.4246 2.74664 20.8388L21.8385 1.74696C22.4243 1.16117 23.3741 1.16117 23.9598 1.74696Z" fill="black"/>
           <path fill-rule="evenodd" clip-rule="evenodd" d="M2.03993 1.74694C2.62571 1.16115 3.57546 1.16115 4.16125 1.74694L23.2531 20.8388C23.8389 21.4246 23.8389 22.3744 23.2531 22.9601C22.6673 23.5459 21.7176 23.5459 21.1318 22.9601L2.03993 3.86826C1.45414 3.28247 1.45414 2.33273 2.03993 1.74694Z" fill="black"/>
@@ -32,12 +32,40 @@ export default {
       name: '',
       email: '',
       password: '',
-      isSignUp: false
+      isDrag: false,
+      isClose: false,
+      time: '',
+      count: 1,
     }
   },
   methods: {
     close() {
+      this.isClose = true;
+      this.time = setInterval(() => {this.closeBox()}, 1000);
+      // this.$emit("my-click2", true);
+    },
+    closeBox() {
+      if(this.count > 0) {
+        this.count--;
+      } else if (this.count === 0){
+        clearInterval(this.time);
+      } 
       this.$emit("my-click2", true);
+    },
+    dragIn() {
+      if(!this.isDrag) {
+        this.isDrag = true;
+        this.time = setInterval(() => {this.shakeBox()}, 1000);
+      }
+    },
+    shakeBox() {
+      if(this.count > 0) {
+        // this.isDrag = true;
+        this.count--;
+      } else if (this.count === 0){
+        clearInterval(this.time);
+      }
+      this.count = 1;
     },
     login() {
       this.$emit("my-click", true);
@@ -88,13 +116,62 @@ export default {
   gap: 0.5rem;
   backdrop-filter: blur(3px);
 }
+.isActive {
+  animation: skew 1.2s;
+}
+.isActive2 {
+  animation: skew2 1.2s;
+}
 .close {
   position: absolute;
   top: 0;
   right: 0;
   border: none;
+  background-color: rgba(0, 0, 0, 0);
+  transition: 0.5s;
+  /* transition: 1s; */
 }
-
+.close:hover {
+  transform: skew(-45deg, 45deg);
+  scale: 0.6;
+}
+@keyframes skew {
+  0% {
+    transform: skew(-5deg, 5deg);
+  }
+  25% {
+    transform: skew(5deg, -5deg);
+  }
+  50% {
+    transform: skew(-3deg, 3deg);
+  }
+  73% {
+    transform: skew(3deg, -3deg);
+  }
+  100% {
+    transform: skew(0, 0);
+  }
+}
+@keyframes skew2 {
+  0% {
+    transform: skew(5deg, -5deg);
+  }
+  25% {
+    transform: skew(-5deg, 5deg);
+  }
+  50% {
+    transform: skew(5deg, -5deg);
+    transform: translateY(10px);
+  }
+  75% {
+    transform: translateY(50px);
+    opacity: 0.8;
+  }
+  100% {
+    transform: translateY(500px);
+    opacity: 0;
+  }
+}
 h2 {
   margin-bottom: 2rem;
 }
--- src/components/timer_comp/TimerComp.vue
+++ src/components/timer_comp/TimerComp.vue
@@ -1,24 +1,64 @@
 <template>
-  <div class="timer" >
-    <div class="header">
-      <button @click="toUserPage">{{ userName }}</button>
-    </div>
-    <div class="watch__wrapper">
-      <p class="text" :class="{count__now:isCount && m > 0}">{{ m }}</p>
-      <p class="text" :class="{count__now:isCount && (s > 0 || m > 0)}">{{ s }}</p>
-      <p class="text" :class="{count__now:isCount}">{{ ms }}</p>
-    </div>
-    <div class="btn__circle">
-      <button></button>
-      <div class="inner__btn">
-        <button></button>
-        <button></button>
-        <button></button>
-      </div>
-      <button></button>
-    </div>
+  <div class="outer">
     <TimerSettingComp v-show="isSetting" class="setting"></TimerSettingComp>
-  </div><!--timer-->
+    <div class="timer" :class="{isActive:isSetting}">
+      <div class="watch__wrapper">
+        <p class="text" :class="{count__now:isCount && m > 0}">{{ m }}</p>
+        <p class="text" :class="{count__now:isCount && (s > 0 || m > 0)}">{{ s }}</p>
+        <p class="text" :class="{count__now:isCount}">{{ ms }}</p>
+      </div>
+      <div class="controller">
+        <div class="pm">
+          <span>
+            <svg width="58" height="52" viewBox="0 0 58 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_i_60_49)">
+            <path d="M24.493 2.5C26.4175 -0.833332 31.2288 -0.833335 33.1533 2.5L56.969 43.75C58.8935 47.0833 56.4879 51.25 52.6389 51.25H5.00746C1.15846 51.25 -1.24717 47.0833 0.677332 43.75L24.493 2.5Z" fill="#EAEAEA" fill-opacity="0.5"/>
+            </g>
+            <defs>
+            <filter id="filter0_i_60_49" x="0" y="0" width="57.6465" height="51.25" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset/>
+            <feGaussianBlur stdDeviation="2.5"/>
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="shape" result="effect1_innerShadow_60_49"/>
+            </filter>
+            </defs>
+            </svg>
+          </span>
+          <span>
+            <svg width="58" height="52" viewBox="0 0 58 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_i_60_50)">
+            <path d="M33.1535 48.75C31.229 52.0833 26.4177 52.0833 24.4932 48.75L0.677502 7.50001C-1.247 4.16668 1.15862 4.93833e-06 5.00762 4.60184e-06L52.639 4.37766e-07C56.488 1.01276e-07 58.8936 4.16667 56.9691 7.5L33.1535 48.75Z" fill="#EAEAEA" fill-opacity="0.5"/>
+            </g>
+            <defs>
+            <filter id="filter0_i_60_50" x="0" y="0" width="57.6465" height="51.25" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset/>
+            <feGaussianBlur stdDeviation="2.5"/>
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="shape" result="effect1_innerShadow_60_50"/>
+            </filter>
+            </defs>
+            </svg>
+          </span>
+        </div>
+        <button class="pm__set"></button>
+        <div class="ssr">
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+      <div class="footer">
+        <button @touchstart="userPage"></button>
+      </div>
+    </div><!--timer-->
+  </div>
   
 </template>
 
@@ -30,6 +70,7 @@ export default {
   },
   data() {
     return {
+      count:0,
       timerName: 'defalt',
       userName: 'Who',
       time: '',
@@ -64,12 +105,44 @@ export default {
     },
     toTimerSetting() {
       this.isSetting = !this.isSetting;
+    },
+    userPage() {
+      console.log("touch");
+      this.isSetting = true;
     }
   }
 }
 </script>
 
 <style scoped>
+.outer {
+  position: relative;
+}
+.setting {
+  display: block;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  background-color: ghostwhite;
+  z-index: 100;
+  animation: downIn 1.5s;
+}
+@keyframes downIn {
+  0% {
+    opacity: 0;
+    transform: translateY(100px);
+    scale: 0.7;
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(200px);
+  }
+}
 .timer {
   position: relative;
   width: 100%;
@@ -79,21 +152,22 @@ export default {
   justify-content: center;
   background-color: rgba(0, 0, 0, 0);
 }
-.header {
+.footer {
   position: absolute;
-  top: 0;
+  bottom: 0;
   left: 0;
   width: 100%;
   display: flex;
-  justify-content: flex-end;
-  padding: 0.5rem;
+  justify-content: flex-start;
+  padding: 0.2rem;
   align-items: center;
   gap: 1rem;
 }
-.header button {
-  width: 20%;
-  height: 50px;
-  border-radius: 2rem 1rem 1rem 2rem /2rem 1rem 1rem 2rem;
+.footer button {
+  width: 80px;
+  height: 70px;
+  border: none;
+  border-radius: 1rem 2rem 2rem 1rem /1rem 2rem 2rem 1rem;
   background-color: rgba(252, 252, 232, 0.7);
 }
 .watch__wrapper {
@@ -132,70 +206,48 @@ export default {
   border-style: solid;
   border-radius: 2px;
 }
-.timer .setting {
-  display: block;
-  width: 80%;
-  height: 80%;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-  background-color: ghostwhite;
-}
-.btn__circle {
+.controller {
   position: fixed;
-  bottom: 0.5rem;
-  left: 0;
-  right: 0;
-  margin: auto;
+  bottom: 0.2rem;
+  right: 0.2rem;
   width: 200px;
   height: 200px;
   border-radius: 6rem 6rem 2rem 2rem /6rem 6rem 2rem 2rem;
   background-color: rgba(252, 252, 232, 0.5);
   display: flex;
+  justify-content: space-evenly; 
+  align-items: center; 
+}
+.pm {
+  height: 100%;
+  display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
-  gap: 5px;
+  gap: 0.5rem;
 }
-.btn__circle button {
-  width: 65px;
-  height: 65px;
-  border-radius: 1rem;
+.pm__set {
+  margin-top: 2.5rem;
 }
-.btn__circle div {
-  position: relative;
+.ssr {
+  height: 100%;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: end;
+  gap: 2rem;
   align-items: center;
-  gap: 5px;
+  margin-bottom: 2rem;
 }
-.inner__btn button {
-  width: 70px;
-  height: 70px;
+.ssr button:last-child {
+  background-color: red;
+  width: 30px;
+  height: 30px;
+}
+button {
+  width: 40px;
+  height: 40px;
+  border: solid 1px grey;
   border-radius: 50%;
 }
-.inner__btn button:first-child {
-  width: 55px;
-  height: 55px;
-}
-.inner__btn button:first-child::after {
-  content: '';
-  width: 2rem;
-  position: absolute;
-  left: 0.8rem;
-  bottom: 2rem;
-  margin: auto;
-  border-width: 0 0 5px;
-  border-style: solid;
-  border-radius: 2px;
-  color: rgba(0, 255, 4, 0.9);
-}
-.inner__btn button:last-child {
-  width: 55px;
-  height: 55px;
-  background-color: rgb(255, 93, 93);
-}
+
+
 </style>
